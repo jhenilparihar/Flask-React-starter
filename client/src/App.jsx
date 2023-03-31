b90b488a235c535117ec062f232d12fac9f9fa85
Jhenil Parihar <jhenilparihar@gmail.com>
2023-03-31 23:57:48 +0530

    created routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,31 @@
 import { useState } from "react";
-import "./App.css";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
+import "./App.css";
 import TodoPage from "./pages/TodoPage";
+import TodoItem from "./pages/TodoItem";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <TodoPage />,
+    },
+    {
+      path: "/todo/:id",
+      element: <TodoItem />,
+    },
+  ]);
+
   return (
     <div className="App">
-      <TodoPage />
+      <RouterProvider router={router} />
     </div>
   );
 }
